a6e354e68958ceff31385a8421dc7b3478ddb80e
w15836572892-arch <w15836572892@gmail.com>
2026-07-08 18:32:25 +0800

    新增.java

--- src/main/ddddddddddd.java
+++ src/main/ddddddddddd.java
@@ -1,1 +1,9 @@
-hhhhhhhhh
+public class PrintDemo2 {
+    public static void main(String[] args) {
+
+        String text = "我是反击开";
+
+        System.out.println(text);
+        System.out.println("我是反击开");
+    }
+}
--- src/main/demo01.java
+++ src/main/demo01.java
@@ -2,5 +2,6 @@ public class PrintDemo1 {
     public static void main(String[] args) {
         System.out.println("小凯真的凯");
         System.out.println("小凯真的凯");
+        System.out.println("来杯小凯摇一摇");
     }
 }
