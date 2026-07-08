2eef82b368006210110f7466e27c5232a141837a
w15836572892-arch <w15836572892@gmail.com>
2026-07-08 17:35:28 +0800

    新增3个Java基础打印示例，分别实现直接输出、变量存储、循环打印"小凯真的凯"

--- KKK.java
+++ KKK.java
@@ -1,1 +1,10 @@
-hhhhhhhhhhhhhhhhhhh
+
+public class PrintDemo3 {
+    public static void main(String[] args) {
+        String words = "小凯真的凯";
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(words);
+        }
+    }
+}
