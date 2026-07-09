6e8d27dd18e647e18141353ab901f0481071efd4
abc <abc@qq.com>
2026-07-09 14:33:20 +0800

    为6行打印加入“吗？”

---
+++ PrintDemo3.java
@@ -0,0 +1,12 @@
+
+public class PrintDemo3 {
+    public static void main(String[] args) {
+        String words = "小凯真的凯";
+
+        System.out.println("小凯真的凯吗？");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(words);
+        }
+    }
+}
