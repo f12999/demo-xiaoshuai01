baf4addcea427655c449ee1cd0094c63b09afdf1
XINXIN-cc <cxy19737229779@qq.com>
2026-07-09 21:26:33 +0800

    无修改
    Fix syntax error in print statement.

--- KKK.java
+++ KKK.java
@@ -4,7 +4,7 @@ public class PrintDemo3 {
         String words = "小凯真的凯";
 
         System.out.println("小凯真的凯");
-  System.out.println("小帅真的帅);
+
         for (int i = 0; i < 3; i++) {
             System.out.println(words);
         }
